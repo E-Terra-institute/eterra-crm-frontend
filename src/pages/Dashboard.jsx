6a4d221ca3856f8c0d6ef4318069c4078aea30c4
E-Terra-institute <institute@e-terra.com.ua>
2025-07-16 02:35:37 +0300

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Заявки</h2>
+      {/* Тут буде таблиця заявок */}
+    </div>
+  );
+}
+
+export default Dashboard;
